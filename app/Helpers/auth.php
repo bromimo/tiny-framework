14b75567bf95ef6b21c17e168ce298ed8a1c0bd8
broveymimo <k.arzhanov@alef.im>
2026-02-25 18:07:03 +0200

    feat: add Bearer token auth helper

---
+++ app/Helpers/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+function getBearerToken(): ?string
+{
+    $header = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
+
+    if (preg_match('/^Bearer\s+(.+)$/i', $header, $matches)) {
+        return trim($matches[1]);
+    }
+
+    return null;
+}
